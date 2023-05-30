a007e00414622cb36c721ce240d31eb386dc92bd
Wallacy <wallacy_vinicius@hotmail.com>
2023-05-30 02:40:38 -0300

    exercicio-condicionais-II

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,24 @@
+// # Exercício 2
+
+// Vamos **recriar** o exercício *Nacionalidade* da aula de Condicionais I, dessa vez utilizando o **switch case**.
+// No arquivo `script.js` existe uma versão do código já escrita, mas você pode colar o exercício que você fez na última prática! 😉
+
+// -> Relembrando o enunciado:
+
+// Crie uma função que receba do usuário sua nacionalidade, utilizando um prompt que diga: "Escreva aqui sua nacionalidade". Guarde o valor da resposta em uma const nacionalidade.
+
+// Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+
+// - brasileira;
+// - argentina;
+// - uruguaia;
+// - chilena;
+// - colombiana;
+
+// Crie uma estrutura com o **switch case** que verifique se a nacionalidade recebida é igual a alguma das nacionalidades acima. Caso seja, imprima a nacionalidade no console.
+// O programa deve imprimir "nacionalidade não encontrada" caso o valor de nacionalidade não corresponda a nenhum das opções acima.
+
+
 let nacionalidade = prompt("Digite aqui a nacionalidade").toLowerCase()
 
 if(nacionalidade === "brasileira"){
@@ -13,3 +34,30 @@ if(nacionalidade === "brasileira"){
 } else{
     console.log("nacionalidade não encontrada")
 }
+
+
+switch(nacionalidade){
+    case "brasileira":
+    case "brasileiro":
+    console.log("A pessoa é do Brasil !");
+    break;
+    case "argentina":
+    case "argentino":
+    console.log("A pessoa é da Argentina !");
+    break;
+    case "uruguaia":
+    case "uruguaio":
+    console.log("A pessoa é do Uruguai !");
+    break;
+    case "chilena":
+    case "chileno":
+    console.log("A pessoa é do Chile !");
+    break;
+    case "colombiana":
+    case "colombiano":
+    console.log("A pessoa é da Colômbia !");
+    break;
+    default:
+    console.log("Nacionalidade não encontrada");
+}
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,24 @@
+// # Exercício 3
+
+// Reescreva os códigos do arquivo `script.js` utilizando o **if ternário**.
+
+// **Relembrando o if ternário**
+// ```jsx
+// condição ? expr1 : expr2
+// ```
+
+// Em que:
+
+// `condição` é uma expressão que é avaliada como `true` ou `false`
+
+// `expr1` e `expr2` são expressões com valores de qualquer tipo
+
+
 //códigos a serem reescritos
 
 // a)
+let nome = "José"
+
 if(nome === "José"){
 	console.log("Oi, Zé!")
 } else {
@@ -8,9 +26,20 @@ if(nome === "José"){
 }
 
 
+nome === "José" ?
+console.log("Oi, Zé!") : 
+console.log("Olá, " + nome)
+
+
 // b)
+let idade = 18
+
 if(idade >= 18){
 	console.log("Pode tirar carteira de motorista!")
 } else {
 	console.log("Ainda não pode tirar carteira de motorista!")
 }
+
+idade >= 18 ?
+console.log("Pode tirar carteira de motorista!") :
+console.log("Ainda não pode tirar carteira de motorista!")
